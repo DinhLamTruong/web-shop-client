3e41022ca1e210ae77e576774c1e47c4a855cedf
truong <anhtrangdep012@gmail.cocm>
2024-11-11 18:05:37 +0700

    done-njs03-client

--- src/Detail/Detail.jsx
+++ src/Detail/Detail.jsx
@@ -18,108 +18,7 @@ function Detail(props) {
   //id params cho từng sản phẩm
   const { id } = useParams();
 
-  //id_user được lấy từ redux
-  // const id_user = useSelector(state => state.Cart.id_user);
-
-  //listCart được lấy từ redux
-  //   const listCart = useSelector(state => state.Cart.listCart);
-
   const [product, setProduct] = useState([]);
-
-  //   const [star, setStar] = useState(1);
-
-  //   const [comment, setComment] = useState('');
-
-  // id_user đã đăng nhập
-  //   const idUser = useSelector(state => state.Session.idUser);
-
-  //   // Listcomment
-  //   const [list_comment, set_list_comment] = useState([]);
-
-  //   // state này dùng để load lại comment khi user gửi comment lên
-  //   const [load_comment, set_load_comment] = useState(false);
-
-  //   // Hàm này dùng để lấy dữ liệu comment
-  //   // Hàm này sẽ chạy lại phụ thuộc vào id Param
-  //   useEffect(() => {
-  //     const fetchData = async () => {
-  //       const params = {
-  //         idProduct: id,
-  //       };
-
-  //       const query = '?' + queryString.stringify(params);
-
-  //       const response = await CommentAPI.getCommentProduct(query);
-  //       console.log(response);
-
-  //       set_list_comment(response);
-  //     };
-
-  //     fetchData();
-  //   }, [id]);
-
-  //   // Hàm thay đổi sao đánh giá
-  //   const onChangeStar = e => {
-  //     setStar(e.target.value);
-  //   };
-
-  //   // Hàm thay đổi comment
-  //   const onChangeComment = e => {
-  //     setComment(e.target.value);
-  //   };
-
-  //   // Hàm này dùng để bình luận
-  //   const handlerComment = () => {
-  //     if (idUser === '') {
-  //       alertify.set('notifier', 'position', 'bottom-left');
-  //       alertify.error('Vui Lòng Kiểm Tra Đăng Nhập!');
-  //       return;
-  //     }
-
-  //     const fetchSendComment = async () => {
-  //       const params = {
-  //         idProduct: id,
-  //         idUser: localStorage.getItem('id_user'),
-  //         fullname: localStorage.getItem('name_user'),
-  //         content: comment,
-  //         star: star,
-  //       };
-
-  //       const query = '?' + queryString.stringify(params);
-
-  //       const response = await CommentAPI.postCommentProduct(query);
-  //       console.log(response);
-
-  //       set_load_comment(true);
-  //     };
-
-  //     fetchSendComment();
-
-  //     setComment('');
-  //   };
-
-  //   // Hàm này dùng để load lại dữ liệu comment
-  //   // Phụ thuộc vào state load_comment
-  //   useEffect(() => {
-  //     if (load_comment) {
-  //       const fetchData = async () => {
-  //         const params = {
-  //           idProduct: id,
-  //         };
-
-  //         const query = '?' + queryString.stringify(params);
-
-  //         const response = await CommentAPI.getCommentProduct(query);
-  //         console.log(response);
-
-  //         set_list_comment(response);
-  //       };
-
-  //       fetchData();
-
-  //       set_load_comment(false);
-  //     }
-  //   }, [load_comment]);
 
   //Hàm này gọi API và cắt chỉ lấy 4 sản phẩm
   useEffect(() => {
@@ -230,10 +129,6 @@ function Detail(props) {
 
       fetchPost();
     }
-    // else {
-    //   const action = addCart(data);
-    //   dispatch(action);
-    // }
   };
 
   return (
@@ -380,131 +275,21 @@ function Detail(props) {
             </div>
           </div>
         </div>
-        {/* <div className='form-group'>
-					<label htmlFor='exampleFormControlTextarea1'>Comment:</label>
-					<textarea
-						className='form-control'
-						rows='3'
-						onChange={onChangeComment}
-						value={comment}></textarea>
-				</div> */}
-        {/* <div className='d-flex justify-content-between'>
-					<div className='d-flex w-25'>
-						<span className='mt-2'>Evaluate: </span>
-						&nbsp; &nbsp;
-						<input
-							className='form-control w-25'
-							type='number'
-							min='1'
-							max='5'
-							value={star}
-							onChange={onChangeStar}
-						/>
-						&nbsp; &nbsp;
-						<span className='mt-2'>Star</span>
-					</div>
-					<div>
-						<a
-							className='btn btn-dark btn-sm btn-block px-0 text-white'
-							style={{ width: '12rem' }}
-							onClick={handlerComment}>
-							Send
-						</a>
-					</div>
-				</div> */}
+
         <br />
-        <ul className="nav nav-tabs border-0">
-          {/* <li className="nav-item">
-            <a
-              className="nav-link fix_comment"
-              onClick={() => handlerReview('description')}
-              style={
-                review === 'description'
-                  ? { backgroundColor: '#383838', color: '#ffffff' }
-                  : { color: '#383838' }
-              }
+
+        <div className="tab-pane fade show active">
+          <div className="pt-4 pb-4 bg-white">
+            <h6 className="text-uppercase">Product description </h6>
+            <br></br>
+            <p
+              className="text-muted text-small mb-0"
+              style={{ whiteSpace: 'pre-wrap' }}
             >
-              Description
-            </a>
-          </li> */}
-          {/* <li className='nav-item'>
-						<a
-							className='nav-link fix_comment'
-							onClick={() => handlerReview('review')}
-							style={
-								review === 'review'
-									? { backgroundColor: '#383838', color: '#ffffff' }
-									: { color: '#383838' }
-							}>
-							Reviews
-						</a>
-					</li> */}
-        </ul>
-        {/* <div className="tab-content mb-5">
-          {review === 'description' ? (
-            <div className="tab-pane fade show active">
-              <div className="pt-4 pb-4 bg-white">
-                <h6 className="text-uppercase">Product description </h6>
-                <br></br>
-                <p
-                  className="text-muted text-small mb-0"
-                  style={{ whiteSpace: 'pre-wrap' }}
-                >
-                  {detail.long_desc}
-                </p>
-              </div>
-            </div>
-          ) : (
-			
-            // <div className="tab-pane fade show active">
-            //   <div className="p-4 p-lg-5 bg-white">
-            //     <div className="row">
-            //       <div className="col-lg-8">
-            //         {list_comment &&
-            //           list_comment.map(value => (
-            //             <div className="media mb-3" key={value._id}>
-            //               <img
-            //                 className="rounded-circle"
-            //                 src="https://img.icons8.com/color/36/000000/administrator-male.png"
-            //                 alt=""
-            //                 width="50"
-            //               />
-            //               <div className="media-body ml-3">
-            //                 <h6 className="mb-0 text-uppercase">
-            //                   {value.fullname}
-            //                 </h6>
-            //                 <p className="small text-muted mb-0 text-uppercase">
-            //                   dd/mm/yyyy
-            //                 </p>
-            //                 <ul className="list-inline mb-1 text-xs">
-            //                   <li className="list-inline-item m-0">
-            //                     <i className={value.star1}></i>
-            //                   </li>
-            //                   <li className="list-inline-item m-0">
-            //                     <i className={value.star2}></i>
-            //                   </li>
-            //                   <li className="list-inline-item m-0">
-            //                     <i className={value.star3}></i>
-            //                   </li>
-            //                   <li className="list-inline-item m-0">
-            //                     <i className={value.star4}></i>
-            //                   </li>
-            //                   <li className="list-inline-item m-0">
-            //                     <i className={value.star5}></i>
-            //                   </li>
-            //                 </ul>
-            //                 <p className="text-small mb-0 text-muted">
-            //                   {value.content}
-            //                 </p>
-            //               </div>
-            //             </div>
-            //           ))}
-            //       </div>
-            //     </div>
-            //   </div>
-            // </div>
-          )}
-        </div> */}
+              {detail.long_desc}
+            </p>
+          </div>
+        </div>
         <h2 className="h5 text-uppercase mb-4">Related products</h2>
         <div className="row">
           {product &&
