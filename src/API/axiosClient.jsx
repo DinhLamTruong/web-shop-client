5de02afccd96259cb81945086516cfa4b3a9eca2
truong <anhtrangdep012@gmail.cocm>
2025-03-31 01:32:10 +0700

    update url web server

--- src/API/axiosClient.jsx
+++ src/API/axiosClient.jsx
@@ -5,7 +5,7 @@ import queryString from 'query-string';
 // Please have a look at here `https://github.com/axios/axios#requestconfig` for the full list of configs
 const axiosClient = axios.create({
   // baseURL: 'http://localhost:5000',
-  baseURL: 'https://asm-njs03-server.onrender.com',
+  baseURL: 'https://web-shop-server.onrender.com',
   headers: {
     'content-type': 'application/json',
   },
