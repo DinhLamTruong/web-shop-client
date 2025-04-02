4b8c5c06f831f062e576c2239574c52840e57458
truong <anhtrangdep012@gmail.cocm>
2025-04-02 12:01:12 +0700

    update url web server

--- src/Share/Chat/Chat.jsx
+++ src/Share/Chat/Chat.jsx
@@ -6,7 +6,7 @@ import { io } from 'socket.io-client';
 import ChatRoomsAPI from '../../API/ChatRoomsAPI';
 import './Chat.css';
 
-const socket = io('https://web-shop-server.onrender.com', {
+const socket = io('wss://web-shop-server.onrender.com', {
   transports: ['websocket'],
 });
 
